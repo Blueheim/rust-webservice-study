e553a581bfcf42abe62e19a36c97baf7d2f19a68
blueheim <crystalrod0@gmail.com>
2023-02-22 13:24:49 +0100

    init setup and sqlx

---
+++ domains/src/cat/migrations/20230222101545_cats_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS cats (
+    id serial PRIMARY KEY,
+    name VARCHAR (140) not null,
+    age smallint not null,
+    weight real,
+    created_on TIMESTAMP NOT NULL DEFAULT NOW(),
+); 
